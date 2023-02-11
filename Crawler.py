46837fc326675ba811d88d81413b761d6f20ac17
Saisushmitha <sushmitha201997@gmail.com>
2023-02-11 16:57:29 -0500

    implementd for one month and tested on which regex works the best

--- Crawler.py
+++ Crawler.py
@@ -2,53 +2,55 @@ import re
 import requests
 import sys
 from warcio.archiveiterator import ArchiveIterator
+from io import BytesIO
+import gzip
 
 # regex = re.compile(r"covid.*economic.*impact", re.IGNORECASE)
+# regex = re.compile(r"covid", re.IGNORECASE | re.DOTALL)
 
+#consider this number 1
+# regex = re.compile(r"covid.*(economi|economic).*", re.IGNORECASE | re.DOTALL)
+
+# Provides better results than number 1 #3
+# regex = re.compile(r"covid.*(economic.*impact|impact.*economic)", re.IGNORECASE | re.DOTALL)
+
+# trying with this one #3
+# regex = re.compile(r"covid.*(\beconom.*\b)", re.IGNORECASE | re.DOTALL)
+# Getting only the covid ones
 regex =re.compile(r"covid", re.IGNORECASE | re.DOTALL) 
 
 
-# = re.compile(
-#     "(youtu\.be/|youtube\.com/(watch\?(.*\&)?v=|(embed|v)/))([^?&\"'>]+)"
-# )
 entries = 0
 matching_entries = 0
+base_url = "https://data.commoncrawl.org/"
 
-file_name = "https://data.commoncrawl.org/crawl-data/CC-MAIN-2020-05/segments/1579250589560.16/warc/CC-MAIN-20200117123339-20200117151339-00000.warc.gz"
-
-if len(sys.argv) > 1:
-    file_name = sys.argv[1]
-
-stream = None
-if file_name.startswith("http://") or file_name.startswith(
-    "https://"
-):
-    stream = requests.get(file_name, stream=True).raw
-else:
-    stream = open(file_name, "rb")
+warc_paths_url = "https://data.commoncrawl.org/crawl-data/CC-MAIN-2020-50/warc.paths.gz"
+response = requests.get(warc_paths_url)
+with gzip.open(BytesIO(response.content), "rb") as f:
+    warc_paths = f.read().decode("utf-8").split("\n")
 
 results = []
-
-for record in ArchiveIterator(stream):
-    if record.rec_type == "warcinfo":
+for warc_path in warc_paths:
+    if len(warc_path) == 0:
         continue
+    full_url = base_url + warc_path
+    stream = requests.get(full_url, stream=True).raw
+    for record in ArchiveIterator(stream):
+        if record.rec_type == "warcinfo":
+            continue
 
-    if not ".com/" in record.rec_headers.get_header(
-        "WARC-Target-URI"
-    ):
-        continue
+        if not ".com/" in record.rec_headers.get_header("WARC-Target-URI"):
+            continue
 
-    entries = entries + 1
-    contents = (
-        record.content_stream()
-        .read()
-        .decode("utf-8", "replace")
-    )
-    if regex.search(contents):
-        matching_entries = matching_entries + 1
-        results.append(record.rec_headers.get_header("WARC-Target-URI"))
+        entries = entries + 1
+        contents = (
+            record.content_stream().read().decode("utf-8", "replace")
+        )
+        if regex.search(contents):
+            matching_entries = matching_entries + 1
+            results.append(record.rec_headers.get_header("WARC-Target-URI"))
+    break
 
 print("{} matching pages found in {}/{} entries.".format(len(results), matching_entries, entries))
 for url in results:
     print(url)
-
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SaiSushmithaSanduri
+
+Refernces:
+[1] https://arxiv.org/pdf/2102.09507
+
+Findings:
+
+there are URLs which current give out a 404 error
